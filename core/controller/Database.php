fd554fc86af700c10be7005c4b0e55ca22b56879
Sole <sole.gutierrez@gmail.com>
2019-09-29 00:33:13 +0200

    parametros bd

--- core/controller/Database.php
+++ core/controller/Database.php
@@ -1,24 +1,14 @@
-<?php
+﻿<?php
 class Database {
 	public static $db;
 	public static $con;
 	function Database(){
-		// Entorno de producción
-        if (isset($_ENV["CLEARDB_DATABASE_URL"]))
-        {
-            $cleardb_url     = parse_url(getenv("CLEARDB_DATABASE_URL"));
-            $var_server   = $cleardb_url["host"];
-            $var_username = $cleardb_url["user"];
-            $var_password = $cleardb_url["pass"];
-            $var_db       = substr($cleardb_url["path"],1);
-        }
-        else //Entorno de desarrollo
-        {
-            $var_server   = "localhost";
-            $var_username = "username";
-            $var_password = "userpassword";
-            $var_db       = "newdb";
-        }
+
+            $var_server   = "127.0.0.1";
+            $var_username = "user";
+            $var_password = "pass";
+            $var_db       = "prueba";
+
 		$this->user=$var_username;
 		$this->pass=$var_password;
 		$this->host=$var_server;
